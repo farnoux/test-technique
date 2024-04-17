a973abb5ad9dc43e1d8cf4f69f060752e4ad5838
Fred <98240+farnoux@users.noreply.github.com>
2024-04-17 11:28:14 +0200

    ♻️ Specify column names

--- tests/collectivities.list.test.ts
+++ tests/collectivities.list.test.ts
@@ -18,7 +18,7 @@ describe("List collectivities", async () => {
   test("As an auth user, I can list the collectivities that I am a member of", async () => {
     const { data, error } = await client
       .from("collectivite")
-      .select(`*, membre!inner()`)
+      .select(`id, nom, membre!inner()`)
       .eq("membre.user_id", user.id);
 
     expect(error).toBeNull();
@@ -27,7 +27,7 @@ describe("List collectivities", async () => {
   });
 
   test("As a random user, I can list all the existing collectivities", async () => {
-    const { data, error } = await client.from("collectivite").select(`*`);
+    const { data, error } = await client.from("collectivite").select(`id, nom`);
 
     expect(error).toBeNull();
     expect(data).toBeInstanceOf(Array);
